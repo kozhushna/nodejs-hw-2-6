a1eae0a61b6e60f276af3ca1ec889c4300e490ca
kozhushna <k.kozhushna@gmail.com>
2023-06-21 20:24:27 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,12 +5,25 @@ const router = express.Router();
 const contacts = require('../../models/contacts');
 
 router.get('/', async (req, res, next) => {
-  const result = await contacts.listContacts();
-  res.json(result);
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      return res.status(404).json({ message: 'Not found' });
+    }
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 router.post('/', async (req, res, next) => {
