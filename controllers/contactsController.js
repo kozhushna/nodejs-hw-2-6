96180bc963ebd135be2922444c5b54a86e619fb7
kozhushna <k.kozhushna@gmail.com>
2023-06-22 19:09:17 +0300

    Add schemas

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,14 +1,5 @@
-const Joi = require('joi');
 const contacts = require('../models/contacts');
 const { HttpError, ctrlWrapper } = require('../helpers');
-
-const addSchema = Joi.object({
-  name: Joi.string().min(2).max(30).required(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ua'] } })
-    .required(),
-  phone: Joi.string().min(7).max(17).required(),
-});
 
 const listContacts = async (req, res) => {
   const result = await contacts.listContacts();
@@ -25,10 +16,6 @@ const getContactById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const { error } = addSchema.validate(req.body);
-  if (error) {
-    throw HttpError(400, error.message);
-  }
   const result = await contacts.addContact(req.body);
   res.status(201).json(result);
 };
@@ -43,10 +30,6 @@ const removeContact = async (req, res) => {
 };
 
 const updateContact = async (req, res) => {
-  const { error } = addSchema.validate(req.body);
-  if (error) {
-    throw HttpError(400, error.message);
-  }
   const { contactId } = req.params;
   const result = await contacts.updateContact(contactId, req.body);
   if (!result) {
