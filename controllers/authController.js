9363d78b7534ac08c47e5fd893a919e4f885364c
kozhushna <k.kozhushna@gmail.com>
2023-07-06 18:29:02 +0300

    Add errors list

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,7 +39,7 @@ const login = async (req, res) => {
     id: user._id,
   };
 
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1w' });
 
   res.json({
     token,
