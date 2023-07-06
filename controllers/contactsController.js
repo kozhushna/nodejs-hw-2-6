381794aab2c3f2ed600b8a1a0106067802aa3ecf
kozhushna <k.kozhushna@gmail.com>
2023-07-06 19:33:15 +0300

    Add owner functionality get and add

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -2,7 +2,8 @@ const { Contact } = require('../models/contact');
 const { HttpError, ctrlWrapper } = require('../helpers');
 
 const listContacts = async (req, res) => {
-  const result = await Contact.find();
+  const { _id: owner } = req.user;
+  const result = await Contact.find({ owner });
   res.json(result);
 };
 
@@ -16,7 +17,8 @@ const getContactById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -19,6 +19,7 @@ const authenticate = async (req, res, next) => {
     if (!user) {
       next(HttpError(401));
     }
+    req.user = user;
     next();
   } catch {
     next(HttpError(401));
