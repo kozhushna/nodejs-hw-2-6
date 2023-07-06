79e8a89aca564299fe034d12353693a1ac708b6c
kozhushna <k.kozhushna@gmail.com>
2023-07-06 16:55:33 +0300

    Add token functionality

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,4 +9,6 @@ const router = express.Router();
 
 router.post('/register', validateBody(schemas.registerSchema), ctrl.register);
 
+router.post('/login', validateBody(schemas.loginSchema), ctrl.login);
+
 module.exports = router;
