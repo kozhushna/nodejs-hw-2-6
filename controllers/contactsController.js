e669f9a1ccdc79ad5106d963081dd089b584b963
kozhushna <k.kozhushna@gmail.com>
2023-07-07 13:52:58 +0300

    Add filter

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -3,12 +3,18 @@ const { HttpError, ctrlWrapper } = require('../helpers');
 
 const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite = true } = req.query;
+  console.log(req.query);
+
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, '-createdAt -updatedAt', {
-    skip,
-    limit,
-  }).populate('owner', 'email');
+  const result = await Contact.find(
+    { owner, favorite },
+    '-createdAt -updatedAt',
+    {
+      skip,
+      limit,
+    }
+  ).populate('owner', 'email');
   res.json(result);
 };
 
