4dfe3155d1daea3927615989a786e0b7b20b2f4a
kozhushna <k.kozhushna@gmail.com>
2023-07-17 17:00:53 +0300

    Add small avatar with Jimp

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const gravatar = require('gravatar');
 const path = require('path');
 const fs = require('fs/promises');
+const Jimp = require('jimp');
 
 const { User } = require('../models/user');
 const { HttpError, ctrlWrapper } = require('../helpers');
@@ -85,16 +86,27 @@ const logout = async (req, res) => {
 };
 
 const updateAvatar = async (req, res) => {
+  if (!req.user) {
+    throw HttpError(401);
+  }
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
-
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join('avatars', filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
 
-  res.json({
+  const filePath = path.join(avatarsDir, filename);
+  await Jimp.read(filePath)
+    .then((avatar) => {
+      return avatar.resize(250, 250).write(filePath);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  res.status(200).json({
     avatarURL,
   });
 };
