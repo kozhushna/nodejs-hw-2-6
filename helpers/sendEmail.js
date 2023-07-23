0cf21229d297a8541681f24a6f5c198d04e8a9ba
kozhushna <k.kozhushna@gmail.com>
2023-07-23 13:33:40 +0300

    Update sendEmail.js

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -4,13 +4,6 @@ require('dotenv').config();
 const { SENDER_EMAIL, SENDER_PASSWORD } = process.env;
 
 const transport = nodemailer.createTransport({
-  //   service: 'gmail',
-  //   host: 'smtp.gmail.com',
-  //   port: 587,
-  //   secure: false,
-
-  // sendmail: true,
-  //   newline: 'unix',
   host: 'smtp.ukr.net',
   port: 465,
   secure: true,
@@ -23,24 +16,10 @@ const transport = nodemailer.createTransport({
   },
 });
 
-// const email = {
-//   // from: SENDER_EMAIL,
-//   to: SENDER_EMAIL,
-//   subject: 'Test email',
-//   text: 'Test email',
-// };
-
-// transport
-//   .sendMail(email)
-//   .then(() => console.log('Email send success'))
-//   .catch((error) => console.log(error.message));
-
 const sendEmail = async (data) => {
   const email = { ...data, from: SENDER_EMAIL };
   await transport.sendMail(email);
   return true;
 };
-
-// sendEmail(email);
 
 module.exports = sendEmail;
